b3f44dda75571da8ccf0356fb03a7e157e061542
archieval <Syarchieval@gmail.com>
2022-11-28 23:59:37 +0800

    added paths with children

--- src/authentication/_IndexAuthentication.vue
+++ src/authentication/_IndexAuthentication.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>_Index Authentication</h1>
-  <router-view> </router-view>
+  <router-view />
 </template>
   
   <script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,110 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/index.scss'
 import Home from "./Home.vue"
+import _IndexAuthentication from "./authentication/_IndexAuthentication.vue"
+import Login from "./authentication/Login.vue"
+import Register from "./authentication/Register.vue"
+import AdminLogin from "./authentication/AdminLogin.vue"
+import AdminRegister from "./authentication/AdminRegister.vue"
+import ChangePassword from "./authentication/ChangePassword.vue"
+import ForgotPassword from "./authentication/ForgotPassword.vue"
+import _IndexClient from "./client/_IndexClient.vue"
+import About from "./client/About.vue"
+import Cart from "./client/Cart.vue"
+import EditProfile from "./client/EditProfile.vue"
+import Menu from "./client/Menu.vue"
+import _IndexAdmin from "./admin/_IndexAdmin.vue"
+import CustomerService from "./admin/CustomerService.vue"
+import Dashboard from "./admin/Dashboard.vue"
+import EditProfileAdmin from "./admin/EditProfile.vue"
+import Inventory from "./admin/Inventory.vue"
+
+
+
+
+
+
+
 const routes = [
     {
         path: '/',
         alias: "/home",
         component: Home,
+
+    },
+    {
+        path: '/authentication',
+        component: _IndexAuthentication,
+        children: [
+            {
+                path: 'login',
+                component: Login,
+            },
+            {
+                path: 'register',
+                component: Register,
+            },
+            {
+                path: 'Adminlogin',
+                component: AdminLogin,
+            },
+            {
+                path: 'Adminregister',
+                component: AdminRegister,
+            },
+            {
+                path: 'ChangePassword',
+                component: ChangePassword,
+            },
+            {
+                path: 'ForgotPassword',
+                component: ForgotPassword,
+            }
+        ]
+    },
+    {
+        path: '/client',
+        component: _IndexClient,
+        children: [
+            {
+                path: 'About',
+                component: About,
+            },
+            {
+                path: 'Cart',
+                component: Cart,
+            },
+            {
+                path: 'EditProfile',
+                component: EditProfile,
+            },
+            {
+                path: 'Menu',
+                component: Menu,
+            }
+        ]
+    },
+    {
+        path: '/admin',
+        component: _IndexAdmin,
+        children: [
+            {
+                path: 'CustomerService',
+                component: CustomerService,
+            },
+            {
+                path: 'Dashboard',
+                component: Dashboard,
+            },
+            {
+                path: 'EditProfile',
+                component: EditProfileAdmin,
+            },
+            {
+                path: 'Inventory',
+                component: Inventory,
+            }
+        ]
     }
 ]
 const router = createRouter({
